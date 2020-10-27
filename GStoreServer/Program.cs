1332ae05dd3a64d5e6c83c7c9fe3beb98dbf2255
Rafael Alexandre <r.alexandre99@gmail.com>
2020-10-27 23:39:40 +0000

    change base version of client/server application

---
+++ GStoreServer/Program.cs
@@ -0,0 +1,105 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace gStoreServer {
+    // GStoreServerService is the namespace defined in the protobuf
+    // GStoreServerServiceBase is the generated base implementation of the service
+    public class ServerService : GStoreServerService.GStoreServerServiceBase {
+        private GrpcChannel channel;
+        private Dictionary<string, GStoreClientService.GStoreClientServiceClient> clientMap =
+            new Dictionary<string, GStoreClientService.GStoreClientServiceClient>();
+
+        public ServerService() {
+        }
+
+        public override Task<GStoreClientRegisterReply> Register(
+            GStoreClientRegisterRequest request, ServerCallContext context) {
+            Console.WriteLine("Deadline: " + context.Deadline);
+            Console.WriteLine("Host: " + context.Host);
+            Console.WriteLine("Method: " + context.Method);
+            Console.WriteLine("Peer: " + context.Peer);
+            return Task.FromResult(Reg(request));
+        }
+
+        public override Task<BcastMsgReply> BcastMsg(BcastMsgRequest request, ServerCallContext context) {
+            return Task.FromResult(Bcast(request));
+        }
+
+
+        public BcastMsgReply Bcast(BcastMsgRequest request) {
+            // random wait to simulate slow msg broadcast: Thread.Sleep(5000);
+            Console.WriteLine("msg arrived. lazy server waiting for server admin to press key.");
+            Console.ReadKey();
+            lock (this) {
+                foreach (string nick in clientMap.Keys) {
+                    if (nick != request.Nick) {
+                        try {
+                            clientMap[nick].RecvMsg(new RecvMsgRequest
+                            {
+                                Msg = request.Nick + ": " + request.Msg 
+                            });
+                        } catch (Exception e) {
+                            Console.WriteLine(e.Message);
+                            clientMap.Remove(nick);
+                        }
+                    }
+                }
+            }
+            Console.WriteLine($"Broadcast message {request.Msg} from {request.Nick}");
+            return new BcastMsgReply
+            {
+                Ok = true
+            };
+        }
+
+        public GStoreClientRegisterReply Reg(GStoreClientRegisterRequest request) {
+                channel = GrpcChannel.ForAddress(request.Url);
+                GStoreClientService.GStoreClientServiceClient client =
+                    new GStoreClientService.GStoreClientServiceClient(channel);
+            lock (this) {
+                clientMap.Add(request.Nick, client);
+            }
+            Console.WriteLine($"Registered client {request.Nick} with URL {request.Url}");
+            GStoreClientRegisterReply reply = new GStoreClientRegisterReply();
+            lock (this) {
+                foreach (string nick in clientMap.Keys) {
+                    reply.Users.Add(new User { Nick = nick });
+                }
+            }
+            return reply;
+        }
+    }
+    class Program {
+        
+        public static void Main(string[] args) {
+            const int port = 1001;
+            const string hostname = "localhost";
+            string startupMessage;
+            ServerPort serverPort;
+
+            serverPort = new ServerPort(hostname, port, ServerCredentials.Insecure);
+            startupMessage = "Insecure GStoreServer server listening on port " + port;
+
+            Server server = new Server
+            {
+                Services = { GStoreServerService.BindService(new ServerService()) },
+            Ports = { serverPort }
+        };
+
+            server.Start();
+
+            Console.WriteLine(startupMessage);
+            //Configuring HTTP for client connections in Register method
+            AppContext.SetSwitch(
+  "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            while (true) ;
+        }
+    }
+}
+
